21655cf23489b7d270bf2c57bd3814101a831557
R.V.SARISH <143780630+rvsarish@users.noreply.github.com>
2024-08-01 23:43:39 -0700

    Update constants.js

--- src/constants.js
+++ src/constants.js
@@ -1,5 +1,5 @@
 
-export const BASE_URL = 'https://vercel.com/sarish-rvs-projects/trix-server/7DNQLaMLvT2Bt2uHdywjH1QCREeH'; 
+export const BASE_URL = 'https://trix-server.vercel.app'; 
 export const PRODUCTS_URL = '/api/products';
 export const USERS_URL = '/api/users';
 export const ORDERS_URL = '/api/orders';
